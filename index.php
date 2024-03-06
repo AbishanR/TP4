4303fc6798a5d7216654988f8d6ed332960f266b
AbishanR <159271603+AbishanR@users.noreply.github.com>
2024-03-07 00:04:56 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php session_start(); ?>
+<?php include "vues/header.php";
+
+$uc = empty($_GET['uc']) ? "Accueil" : $_GET['uc'];
+
+switch($uc){
+    case 'Accueil':
+        include('vues/Accueil.php');
+        break;
+    case 'continents':
+        break;
+}
+
+include "vues/footer.php";?>
